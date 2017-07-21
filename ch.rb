b309300d725bd7d4f04819e81fdf3943314797ff
chenchaiah challa <chenchaiah.challas@gmail.com>
2017-07-21 16:58:38 +0530

    ch.rb

---
+++ ch.rb
@@ -0,0 +1,17 @@
+
+
+
+$i = 0
+$num = 5
+
+while $i < $num  do
+   puts("Inside the loop i = #$i" )
+   $i +=1
+end
+This will produce the following result −
+
+Inside the loop i = 0
+Inside the loop i = 1
+Inside the loop i = 2
+Inside the loop i = 3
+Inside the loop i = 4
